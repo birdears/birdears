1b921710be97939784ecf956755979dfa0c07034
Iacchus Mercurius <kassivs@gmail.com>
2017-04-27 19:41:18 -0300

    ok

--- birdears.py
+++ birdears.py
@@ -319,7 +319,7 @@ class QuestionBase:
                     cur_octave += 1
                 # FIXME: Concrete Scale: G#5─A#5─C6─C#7─D#7─F7─G7─G#7 | Chroma Concrete: G#5─A5─A#5─B5─C6─C#6─D6─D#6─E6─F6─F#6─G6
                 # this doesn't happens on chromatic at least when only one octave
-                s
+
                 diatonic[idx] = "{}{}".format(note, cur_octave)
 
         if descending:
