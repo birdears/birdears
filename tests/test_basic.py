3aa08dabb0daffb51eb16a26fc921f3157f9d0dc
Iacchus Mercurius <kassivs@gmail.com>
2017-05-01 00:33:09 -0300

    ok

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -20,8 +20,8 @@ def test_questionclass_chromatic_descending(chromatic=True, descending=True):
     a = Question()
     assert(a)
 
-def test_questionclass_minor_chromatic_descending(mode='minor', chromatic=True, descending=True):
-    a = Question()
+def test_questionclass_minor_chromatic_descending():
+    a = Question(mode='minor', chromatic=True, descending=True)
     assert(a)
 
 def test_questionclass_n_octaves(n_octaves=2):
