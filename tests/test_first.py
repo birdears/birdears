9dfe54e7a8581570dff7bcea579769c37bea93ba
Iacchus Mercurius <kassivs@gmail.com>
2017-04-30 23:42:16 -0300

    tests

--- tests/test_first.py
+++ tests/test_first.py
@@ -4,6 +4,14 @@ def test_questionbase():
     a = QuestionBase()
     assert(a)
 
-def test_questinclass():
+def test_questionclass():
     a = Question()
+    assert(a)
+
+def test_intervalclass():
+    a = Interval(mode='major', tonic='C', interval=4)
+    assert(a)
+
+def test_scaleclass():
+    a = Scale(tonic='C')
     assert(a)
