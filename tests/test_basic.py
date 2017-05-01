cbc2bed1ac1795f9af69e0d3f530b69903d6764b
Iacchus Mercurius <kassivs@gmail.com>
2017-04-30 23:44:53 -0300

    tests

---
+++ tests/test_basic.py
@@ -0,0 +1,17 @@
+from birdears import *
+
+def test_questionbase():
+    a = QuestionBase()
+    assert(a)
+
+def test_questionclass():
+    a = Question()
+    assert(a)
+
+def test_intervalclass():
+    a = Interval(mode='major', tonic='C', octave=4)
+    assert(a)
+
+def test_scaleclass():
+    a = Scale(tonic='C')
+    assert(a)
