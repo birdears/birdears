80a732b0dce1a0bb64de5ea00a74e07067df597b
Iacchus Mercurius <kassivs@gmail.com>
2017-05-30 04:39:19 -0300

    and there we go.

--- birdears/sequence.py
+++ birdears/sequence.py
@@ -77,6 +77,31 @@ class Sequence:
         if self.pos_delay:
             self._wait(self.pos_delay)
 
+    def async_play(self, callback):
+        """Plays the Sequence elements of notes and/or chords and wait for
+        `Sequence.pos_delay` seconds.
+        """
+
+        last_idx = len(self.elements) - 1
+
+        for cur_idx, element in enumerate(self.elements):
+
+            # lets leave the last element's delay for pos_delay:
+            delay = self.delay if cur_idx != last_idx else 0
+
+            if type(element) == tuple:
+                el, duration, delay = element
+            else:
+                el = element
+
+            if type(el) == str:
+                self._play_note(el, delay=delay)
+            elif type(el) == list:
+                self._play_chord(el, delay=delay)
+
+        if self.pos_delay:
+            self._wait(self.pos_delay)
+
     def play_element(self, index):
         """Plays element `sequence.elements[index].`
         """
@@ -114,6 +139,66 @@ class Sequence:
         for degree in degrees:
             triad = scale.get_triad(mode=mode, degree=degree)
             self.elements.append(triad)
+
+    def _async_play_note(self, note, duration=None, delay=None):
+        """Plays a note.
+
+        Args:
+            note (str): The note and octave to be played. Eg.: 'C4'
+            duration (float): Duration of the note in seconds.
+            delay (float): Delay after the note in seconds.
+        """
+
+        # requires sox to be installed
+
+        duration = self.duration if duration is None else duration
+        delay = self.delay if delay is None else delay
+
+        command = (
+            "play -V1 -qn synth {duration} pluck {note}"
+            " fade l 0 {duration} 2 reverb"
+        ).format(note=note, duration=duration)
+
+        subprocess.Popen(command.split())
+
+        if delay:
+            self._wait(delay)
+
+    def _async_play_chord(self, chord, duration=None, delay=None):
+        """Plays a chord.
+
+        Args:
+            chord (array_type): An array of pitches (notes and octaves)
+                to be played, representing a chord. Eg.: ['C4', 'Eb4', 'G5']
+            duration (float): Duration of the chord in seconds.
+            delay (float): Delay after the chord in seconds.
+        """
+
+        duration = self.duration if duration is None else duration
+        delay = self.delay if delay is None else delay
+
+        chord_plucks = str()
+        for note in chord:
+            chord_plucks += " pluck {} ".format(note)
+
+        command = (
+            "play -V1 -qn synth {duration} {chord}"
+            " fade l 0 {duration} 2 reverb"
+        ).format(note=note, duration=duration, chord=chord_plucks)
+
+        subprocess.Popen(command.split())
+
+        if delay:
+            self._wait(delay)
+
+    def _async_wait(self, seconds):
+        """Waits, ie., stops execution for some time.
+
+        Args:
+            seconds (float): Seconds to wait.
+        """
+
+        time.sleep(seconds)
 
     def _play_note(self, note, duration=None, delay=None):
         """Plays a note.
