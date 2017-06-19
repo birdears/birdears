e2ad014a37f64f744140ec9b9df59e128ad64ad0
Iacchus Mercurius <kassivs@gmail.com>
2017-06-19 06:29:09 -0300

    docs. ok,..going..

--- birdears/logger.py
+++ birdears/logger.py
@@ -1,31 +1,29 @@
 """
 This submodule exports `logger` to log events.
 
-Logging messages which are less severe than `lvl` will be ignored
+Logging messages which are less severe than `lvl` will be ignored::
 
-```
-Level       Numeric value
------       -------------
-CRITICAL    50
-ERROR       40
-WARNING     30
-INFO        20
-DEBUG       10
-NOTSET      0
+    Level       Numeric value
+    -----       -------------
+    CRITICAL    50
+    ERROR       40
+    WARNING     30
+    INFO        20
+    DEBUG       10
+    NOTSET      0
 
-Level       When it’s used
------       --------------
-DEBUG       Detailed information, typically of interest only when diagnosing
-                problems.
-INFO        Confirmation that things are working as expected.
-WARNING     An indication that something unexpected happened, or indicative of
-                some problem in the near future (e.g. ‘disk space low’).
-                The software is still working as expected.
-ERROR       Due to a more serious problem, the software has not been able to
-                perform some function.
-CRITICAL	A serious error, indicating that the program itself may be unable
-                to continue running.
-```
+    Level       When it’s used
+    -----       --------------
+    DEBUG       Detailed information, typically of interest only when diagnosing
+                    problems.
+    INFO        Confirmation that things are working as expected.
+    WARNING     An indication that something unexpected happened, or indicative of
+                    some problem in the near future (e.g. ‘disk space low’).
+                    The software is still working as expected.
+    ERROR       Due to a more serious problem, the software has not been able to
+                    perform some function.
+    CRITICAL	A serious error, indicating that the program itself may be unable
+                    to continue running.
 """
 
 import logging
