a913619703742a8996031acd87e089d212fb2383
Iacchus Mercurius <kassivs@gmail.com>
2018-03-12 09:43:53 -0300

    going

--- birdears/note.py
+++
@@ -1,8 +0,0 @@
-from .. import CHROMATIC_SHARP
-from .. import CHROMATIC_FLAT
-
-class Note:
-
-    def __init__(self, note):
-        if note in CHROMATIC_SHARP or note in CHROMATIC_FLAT:
-            self.note = note
