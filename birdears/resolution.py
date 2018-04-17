efea190a74d6e9c362bd99cc7766673eb077f73d
Iacchus Mercurius <kassivs@gmail.com>
2018-04-17 06:01:45 -0300

    resolution almost completely refactored

--- birdears/resolution.py
+++ birdears/resolution.py
@@ -5,6 +5,9 @@ from .scale import ChromaticScale
 
 from .sequence import Sequence
 
+from .note_and_pitch import get_pitch_by_number
+from .note_and_pitch import Chord
+
 from functools import wraps
 
 RESOLUTION_METHODS = {}
@@ -71,88 +74,69 @@ def nearest_tonic(question):
 
     # global DIATONIC_MODES, MAX_SEMITONES_RESOLVE_BELOW
 
-    mode = question.mode
-    tonic = question.tonic
+    # mode = question.mode
+    tonic_pitch = question.tonic
     scale = question.scale
+    random_pitch = question.random_pitch
 
-    return Sequence(elements=sequence_list, delay=delay, pos_delay=pos_delay)
+    duration = question.durations['resol']['duration']
+    delay = question.durations['resol']['delay']
+    pos_delay = question.durations['resol']['pos_delay']
+    
+    # this function will receive: tonic, scale and random_pitch (which may be
+    # chromatic, ie., not in `scale`)
 
-#@register_resolution_method
-#def nearest_tonic(question):
-#    """Resolution method that resolve the intervals to their nearest tonics.
-#
-#    Args:
-#        question (obj): Question object from which to generate the
-#            resolution sequence. (this is provided by the `Prequestion` class
-#            when it is `__call__`ed)
-#    """
-#
-#    global DIATONIC_MODES, MAX_SEMITONES_RESOLVE_BELOW
-#
-#    mode = question.mode
-#    tonic = question.tonic
-#    if not hasattr(question, 'question_phrase_intervals'):
-#        intervals = question.interval
-#    else:
-#        intervals = question.question_phrase_intervals
-#    duration = question.durations['resol']['duration']
-#    delay = question.durations['resol']['delay']
-#    pos_delay = question.durations['resol']['pos_delay']
-#    harmonic = question.is_harmonic
-#    descending = question.is_descending
-#
-#    sequence_list = []
-#
-#    if type(intervals) is not list:
-#        intervals = [intervals]
-#
-#    for interval in intervals:
-#        resolution_pitch = []
-#        scale_pitch = DiatonicScale(tonic=tonic, mode=mode,
-#                                    octave=interval.interval_octave,
-#                                    descending=descending)
-#
-#        if interval.chromatic_offset <= MAX_SEMITONES_RESOLVE_BELOW:
-#            begin_to_diatonic = slice(None, interval.diatonic_index + 1)
-#            # resolution_pitch.extend(scale_pitch.scale[begin_to_diatonic])
-#            resolution_pitch.extend(scale_pitch[begin_to_diatonic])
-#            if interval.is_chromatic:
-#                resolution_pitch.append(interval.note_and_octave)
-#            resolution_pitch.reverse()
-#        else:
-#            diatonic_to_end = slice(interval.diatonic_index, None)
-#            if interval.is_chromatic:
-#                resolution_pitch.append(interval.note_and_octave)
-#            resolution_pitch.extend(scale_pitch.scale[diatonic_to_end])
-#
-#        # unisson and octave
-#        if interval.semitones == 0:
-#            resolution_pitch.append(scale_pitch.scale[0])
-#
-#        elif interval.semitones % 12 == 0:
-#            # FIXME: multipe octaves
-#            resolution_pitch.append("{}{}".format(tonic,
-#                                    interval.tonic_octave))
-#
-#        if harmonic:
-#            seq = [[interval.tonic_note_and_octave, x]
-#                   for x in resolution_pitch]
-#        else:
-#            seq = resolution_pitch
-#
-#        seq_list = list()
-#        last_el_idx = len(seq) - 1
-#        for cur_el_idx, cur_el in enumerate(seq):
-#            cur_delay = delay if last_el_idx != cur_el_idx else pos_delay
-#            cur_tuple = (cur_el, duration, cur_delay)
-#            seq_list.append(cur_tuple)
-#
-#        cur_delay = delay if last_el_idx != cur_el_idx else pos_delay
-#        cur_tuple = (cur_el, duration, cur_delay)
-#
-#        sequence_list.extend(seq_list)
-#
-#    return Sequence(elements=sequence_list, delay=delay, pos_delay=pos_delay)
+    # scale = DiatonicScale(tonic='C', mode='major', octave=4, n_octaves=2,
+    # descending=True) # this comes from Jupyter testing
+    
+    #tonic_pitch = scale[0]
+    #random_pitch = choice(scale)
+    
+    # lets create an scale with same tonic, in the octave of the random_pitch
+    scale_random_pitch = DiatonicScale(tonic=tonic_pitch.note,
+                                       octave=random_pitch.octave, n_octaves=1)
+     
+    
+    interval = Interval(tonic_pitch, random_pitch)
+    
+    semitones = interval['distance']['semitones']
+    
+    direction = -1 if semitones <= MAX_SEMITONES_RESOLVE_BELOW else +1
+    
+    resolution = []
+    
+    if random_pitch not in scale:  # random_pitch is chromatic
+        resolution.append(random_pitch)
+        #  next ones above or below are in the diatonic for sure, then:
+        nearest_diatonic_pitch = \
+            get_pitch_by_number(int(random_pitch)+direction)
+    else:
+        nearest_diatonic_pitch = random_pitch  # random_pitch is diatonic
+    
+    nearest_tonic_index = 0 if semitones <= MAX_SEMITONES_RESOLVE_BELOW else -1
+    nearest_tonic_pitch = scale_random_pitch[nearest_tonic_index]
+    
+    tonic_index = scale_random_pitch.index(nearest_tonic_pitch)
+    
+    nearest_diatonic_pitch_index = \
+        scale_random_pitch.index(nearest_diatonic_pitch)
+    
+    ohslice = slice(min(tonic_index, random_pitch_index), max(tonic_index,
+                    nearest_diatonic_pitch_index)+1)
+        
+    resolution = scale_random_pitch[ohslice][::direction]
+    
+    if question.is_harmonic:
+        resolution_harmonic = []
+        
+        for item in resolution:
+            resolution_harmonic.append(Chord(tonic, item))
+        
+        resolution = resolution_harmonic
+    
+    # resolution
+    return Sequence(elements=resolution, duration=duration, delay=delay,
+                    pos_delay=pos_delay)
 
 
 @register_resolution_method
@@ -167,6 +151,7 @@ def repeat_only(question):
             when it is `__call__`ed)
     """
     elements = question.question.elements
+    
     duration = question.durations['resol']['duration']
     delay = question.durations['resol']['delay']
     pos_delay = question.durations['resol']['pos_delay']
