97f3f8866665314658291794699440b442f0839a
Iacchus Mercurius <kassivs@gmail.com>
2018-06-25 02:29:54 -0300

    little bugfix

--- birdears/questionbase.py
+++ birdears/questionbase.py
@@ -162,7 +162,7 @@ class QuestionBase:
         if isinstance(tonic, list) or isinstance(tonic, tuple):
             tonic = choice(tonic)
         elif isinstance(tonic, str) and ',' in tonic:
-            tonic.replace(' ', '')
+            tonic = tonic.replace(' ', '')
             tonic = choice(tonic.split(','))
         elif isinstance(tonic, str) and ('R' in tonic or 'r' in tonic):
             tonic = choice(KEYS)
